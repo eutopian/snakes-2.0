c6ed13cff5bb52b1b4894afa7a7305927e7dac4f
Margaret Ma <eutopia@gmail.com>
2018-04-10 19:52:42 -0400

    work in progress on creating snake components

--- client/App.js
+++ client/App.js
@@ -1,12 +1,61 @@
 import fetch from 'isomorphic-fetch';
 import React, {Component} from 'react';
-import Head from './Head';
-import Body from './Body';
-import Apple from './Apple';
+import GameList from './components/GameList';
+// import Head from './Head';
+// import Body from './Body';
+// import Apple from './Apple';
 
+let gameStore = [];
 
-export default class App extends Component {
+function getInitialState() {
+  return {
+    rows: 20,
+    columns: 20,
+    speed: 300,
+    highScore: 0,
+    snake: {
+      body: [],
+      size: 1,
+      direction: 'r',
+      lost: false,
+      paused: false
+    },
+    gameList: gameStore
+  };
+};
+
+function fetchGames() {
+  return fetch('/games')
+    .then(response => response.json());
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getInitialState()
+    // this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    fetchGames()
+      .then(gameList => {
+        gameStore = gameList;
+        this.setState(
+          { gameList: gameStore }
+        );
+      });
+  }
   render () {
-    return <p>My new snakes</p>
+    return (
+      <div>
+        <p>My new snakes</p>
+
+
+        
+        { this.state.gameList && <GameList gameList={this.state.gameList} /> }
+      </div>
+    );
   }
 }
+
+export default App;
