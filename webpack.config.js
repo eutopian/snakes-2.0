b8d3acad67740e0920161eedd91d5ba35c821b48
Margaret Ma <eutopia@gmail.com>
2018-04-10 12:14:44 -0400

    more webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: 'webpack-bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }, {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loder", "less-loader"]
+      }
+    ]
+  },
+  mode: 'development',
+  devServer: {
+      contentBase: path.resolve(__dirname, "./"),
+      historyApiFallback: true,
+      watchContentBase: true
+  }
+}
